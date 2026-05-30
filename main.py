36de16816f2483acafbf60650d2738bbe78cf3e2
Chitrala Akshitha <22d21a6711akshitha@gmail.com>
2026-05-30 14:51:45 +0530

    Added home route

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-from flask import Flask, request, jsonify, send_file
+from flask import Flask, request, jsonify, send_file, render_template
 from flask_cors import CORS
 
+# ... other imports ...
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/analyze', methods=['POST'])
+def analyze():
 from reportlab.platypus import (
     SimpleDocTemplate,
     Paragraph,
@@ -18,7 +29,6 @@ from question_gen import generate_questions
 
 app = Flask(__name__)
 CORS(app)
-
 
 @app.route('/analyze', methods=['POST'])
 
