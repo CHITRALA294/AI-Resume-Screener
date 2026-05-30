00496d8980ba160db5358e94154384025cfa2e91
Chitrala Akshitha <22d21a6711akshitha@gmail.com>
2026-05-30 14:08:03 +0530

    Connected app.py to main.py

--- app.py
+++ app.py
@@ -1,10 +1,4 @@
-from flask import Flask, render_template
+from main import app
 
-app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run()
